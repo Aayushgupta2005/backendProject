8bac1e1eb90a8e97a028642610fad66a02a083cd
Aayushgupta2005 <aayush23014@iiitd.ac.in>
2025-03-15 15:03:58 +0530

    mongoose cpnnected

--- src/index.js
+++ src/index.js
@@ -9,6 +9,14 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Sever is running at port : ${process.env.PORT || 8000}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed !!!" , err);
+})
 
 
 
